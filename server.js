c90f687b0b69f5125e8f5136f03d26016184c6c5
Wes Bubeck <wesbubeck20@gmail.com>
2017-09-21 22:14:03 -0400

    updated path to the api route

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 //this links the routes
+require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 
