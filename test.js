e7048d193f1f44fab8169131a8d96502fd5c016c
Wes Bubeck <wesbubeck20@gmail.com>
2017-09-22 14:51:33 -0400

    finished the js that will compare your answers to find friends

--- test.js
+++ test.js
@@ -3,6 +3,10 @@ var array1 = [{
     answers: ["1", "2", "3", "4"]
 },
 {
+    name: "Sue",
+    answers: ["3","1","1","2"]
+},
+{
     name: "best friendzy",
     answers: ["3","2","1","2"]
 }]
@@ -20,28 +24,42 @@ var matchDifference = 20;
 
 
 
-var j = 1
+var friendsIndex = 0;
+
+function searchFriends () {
+    for (j = 0; j < array1.length; j++) {
+        friendsIndex = j;
+        personality();
+    }
+    console.log("You perfect match is..... " + match);
+}
 
 function personality () {
 
     answerValue = 0;
 
 for (var i = 0; i < array2[0].answers.length; i++) {
-    var numArray1 = parseInt(array1[j].answers[i]);
+    
+    var numArray1 = parseInt(array1[friendsIndex].answers[i]);
     var numArray2 = parseInt(array2[0].answers[i]);
     
     var questionDifference = Math.abs(numArray1 - numArray2);
 
     answerValue += questionDifference;
 }
-if (answerValue < matchDifference) {
-    matchDifference = answerValue
-    console.log(answerValue);
-    console.log(array1[j].name);
-    console.log(matchDifference)
+
+    if (answerValue < matchDifference) {
+
+        matchDifference = answerValue;
+        match = array1[friendsIndex].name;
+        console.log(answerValue);
+        console.log(array1[friendsIndex].name);
+        console.log(matchDifference)
+    }
+
     
-}
+
 
 }
 
-personality();
+searchFriends();
