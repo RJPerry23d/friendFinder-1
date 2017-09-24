f4ea321e80dfa05596e0e067f1b1457a09b1f230
Wes Bubeck <wesbubeck20@gmail.com>
2017-09-23 22:35:13 -0400

    updating post routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,22 @@ module.exports = function (app) {
     app.get('/api/friends', function (req, res){
         res.json(friendsArray);
     });
-}
+
+      app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    friendsArray.push(req.body);
+      res.json(true);
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friendsArray = [];
+
+    console.log(tableData);
+  });
+};
