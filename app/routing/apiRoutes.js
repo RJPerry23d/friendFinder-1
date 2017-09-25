ea93eb80506166a44b17bb6d1fecc4f2cbf9dcb4
Wes Bubeck <wesbubeck20@gmail.com>
2017-09-24 22:01:33 -0400

    Psuedo coding and removing the clear api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,20 +7,9 @@ module.exports = function (app) {
     });
 
       app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
+    // Note the code here. Our "server" will respond to requests a.
     friendsArray.push(req.body);
       res.json(true);
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    friendsArray = [];
-
-    console.log(tableData);
-  });
 };
