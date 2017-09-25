2b25633b8402f9e8a7c980ad0cbebc60a29afc99
Wes Bubeck <wesbubeck20@gmail.com>
2017-09-24 20:01:55 -0400

    adding photo's

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,18 +3,18 @@
 
 var friendsArray = [{
     name: "Homer Simpson",
-    email: "homer@doe.com",
+    photo: "https://i.pinimg.com/736x/b4/57/78/b45778adb393760d46ae612b7e0d2e5e--homer-simpson-the-simpsons.jpg",
     answers: ["1","2","3","4","5","4","3","2","1","2"]
 },
     
 {
     name: "Barney",
-    email: "barney@doe.com",
+    photo: "https://vignette4.wikia.nocookie.net/simpsons/images/5/5a/We_want_Chilly_Willie.png/revision/latest?cb=20140901153307",
     answers: ["5","2","3","4","5","4","3","2","1","2"]
 },
 {
     name: "Moe",
-    email: "moe@doe.com",
+    photo: "https://i.ytimg.com/vi/Pk8ZqqEZD5w/hqdefault.jpg",
     answers: ["5","5","5","4","5","4","5","5","5","5"]
 }
     
