9bfb535f3b732a920ff48a80da3fc5af9f674cc9
Wes Bubeck <wesbubeck20@gmail.com>
2017-12-01 09:37:17 -0500

    changed data

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,18 +4,18 @@
 var friendsArray = [{
     name: "Homer Simpson",
     photo: "https://i.pinimg.com/736x/b4/57/78/b45778adb393760d46ae612b7e0d2e5e--homer-simpson-the-simpsons.jpg",
-    answers: ["1","2","3","4","5","4","3","2","1","2"]
+    answers: ["5","4","1","4","2","1","3","4","1","3"]
 },
     
 {
     name: "Barney",
     photo: "https://vignette4.wikia.nocookie.net/simpsons/images/5/5a/We_want_Chilly_Willie.png/revision/latest?cb=20140901153307",
-    answers: ["5","2","3","4","5","4","3","2","1","2"]
+    answers: ["2","2","1","2","2","2","5","5","1","2"]
 },
 {
     name: "Moe",
     photo: "https://i.ytimg.com/vi/Pk8ZqqEZD5w/hqdefault.jpg",
-    answers: ["3","3","5","4","1","4","3","5","2","5"]
+    answers: ["4","4","4","4","3","2","3","2","2","4"]
 }
     
 ]
