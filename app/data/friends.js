60a695ae7657b65fea80e824f7f10f17227e6c8a
Wes Bubeck <wesbubeck20@gmail.com>
2017-12-01 11:58:25 -0500

    added characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,36 @@ var friendsArray = [{
     name: "Moe",
     photo: "https://i.ytimg.com/vi/Pk8ZqqEZD5w/hqdefault.jpg",
     answers: ["4","4","4","4","3","2","3","2","2","4"]
+},
+{
+    name: "Bart",
+    photo: "https://upload.wikimedia.org/wikipedia/en/7/70/Bart_Simpson_-_Skateboarding.png",
+    answers: ["5","5","2","3","3","1","2","4","2","2"]
+},
+{
+    name: "Lisa",
+    photo: "https://s3-eu-west-1.amazonaws.com/betty-me/wp-content/uploads/2017/05/28122248/Lisa-Simpson_header-1200x670.png",
+    answers: ["1","4","5","5","2","5","3","1","3","4"]
+},
+{
+    name: "Krusty",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoGR8tkGmbmbJzh4ALNAI-E9o8q0tsW3478FdoJfpLW3Opag5p4Q",
+    answers: ["2","5","1","5","1","3","1","4","1","2"]
+},
+{
+    name: "Flanders",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtTkZGeW6SoM4hAkoirqi6vTH9YUP2Ogr4It_T5Rp4k1MdEpdm",
+    answers: ["5","1","5","1","2","5","5","1","4","5"]
+},
+{
+    name: "Marge",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKlP-PgekjHgTMQMXnndGSkqTXX0-uwqL66pLySU1Jb9jj5aV6DQ",
+    answers: ["2","2","5","2","2","4","5","2","4","3"]
+},
+{
+    name: "Mr. Burns",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT5aUFUWiHlX6Ow-LLcnIbgsjyr7jZ7peXBBvw-u4oQuzcXEP1KQ",
+    answers: ["2","5","4","4","3","2","1","1","4","4"]
 }
     
 ]
